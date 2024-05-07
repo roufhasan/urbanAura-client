e55a6898c012e0ed5283a4cee50df707d58a0376
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2024-05-07 17:16:40 +0600

    gallery image optimized

--- src/Pages/SitePages/Home/Gallery/Gallery.jsx
+++ src/Pages/SitePages/Home/Gallery/Gallery.jsx
@@ -26,46 +26,55 @@ const Gallery = () => {
             src={img1}
             alt="interior design"
             className="col-start-1 col-end-4 row-start-1 row-end-6 h-full w-full object-cover md:col-end-2 md:md:row-start-1 md:md:row-end-7"
+            loading="lazy"
           />
           <img
             src={img2}
             alt="interior design"
             className="col-start-1 col-end-4 row-start-6 row-end-10 h-full w-full object-cover md:col-start-2 md:col-end-6 md:row-start-2 md:row-end-7"
+            loading="lazy"
           />
           <img
             src={img3}
             alt="interior design"
-            className="col-start-4 col-end-10 row-start-3 row-end-11 h-full w-full object-cover md:col-start-1 md:col-end-3 md:row-start-7 md:row-end-13"
+            className="row-end-13h-full col-start-10 col-end-13 row-start-7 w-full object-cover md:col-start-1 md:col-end-3 md:row-start-7 md:row-end-13"
+            loading="lazy"
           />
           <img
             src={img4}
             alt="interior design"
             className="col-start-10 col-end-13 row-start-1 row-end-7 h-full w-full object-cover md:col-start-3 md:col-end-6 md:row-start-7 md:row-end-11"
+            loading="lazy"
           />
           <img
             src={img5}
             alt="interior design"
-            className="col-start-10 col-end-13 row-start-7 row-end-13 h-full w-full object-cover md:col-start-6 md:col-end-8 md:row-start-3 md:row-end-10"
+            className="col-start-4 col-end-10 row-start-3 row-end-11 h-full w-full object-cover md:col-start-6 md:col-end-8 md:row-start-3 md:row-end-10"
+            loading="lazy"
           />
           <img
             src={img6}
             alt="interior design"
             className="hidden h-full w-full object-cover md:col-start-8 md:col-end-11 md:row-start-2 md:row-end-8 md:block"
+            loading="lazy"
           />
           <img
             src={img7}
             alt="interior design"
             className="hidden h-full w-full object-cover md:col-start-11 md:col-end-13 md:row-start-1 md:row-end-8 md:block"
+            loading="lazy"
           />
           <img
             src={img8}
             alt="interior design"
             className="hidden h-full w-full object-cover md:col-start-8 md:col-end-10 md:row-start-8 md:row-end-13 md:block"
+            loading="lazy"
           />
           <img
             src={img9}
             alt="interior design"
             className="hidden h-full w-full object-cover md:col-start-10 md:col-end-12 md:row-start-8 md:row-end-11 md:block"
+            loading="lazy"
           />
         </div>
       </div>
