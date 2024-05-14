85590cbd7fff73a8fe6fef822197ba1c37f845de
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2024-05-14 20:36:23 +0600

    pagination applied

--- src/Pages/SitePages/Shop/Products/Products.jsx
+++ src/Pages/SitePages/Shop/Products/Products.jsx
@@ -1,20 +1,38 @@
 import Card from "../../../../components/Card/Card";
 
-const Products = ({ gridView, sortedProducts }) => {
+const Products = ({
+  gridView,
+  itemsToDisplay,
+  totalPages,
+  handlePageChange,
+}) => {
+  console.log(itemsToDisplay);
   return (
     <section className="px-[4%] pb-[85px] pt-16 md:px-[7%]">
       <div
         className={`grid grid-cols-1 gap-y-8 md:gap-y-10 ${gridView && "md:grid-cols-4 md:gap-x-8"}`}
       >
-        {sortedProducts &&
-          sortedProducts.length > 0 &&
-          sortedProducts.map((product) => (
+        {itemsToDisplay &&
+          itemsToDisplay.length > 0 &&
+          itemsToDisplay.map((product) => (
             <Card key={product.id} product={product} />
           ))}
       </div>
 
+      <div className="mt-8 flex justify-center">
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <button
+            onClick={() => handlePageChange(index + 1)}
+            key={index}
+            className="mx-2 rounded border px-4 py-2"
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+
       {/* pagination */}
-      <div className="mt-[70px] flex items-center justify-center gap-5 text-lg md:gap-9 md:text-xl">
+      {/* <div className="mt-[70px] flex items-center justify-center gap-5 text-lg md:gap-9 md:text-xl">
         <div className="rounded-[10px] bg-[#b88e2f] px-5 py-3 text-white md:px-7 md:py-4">
           1
         </div>
@@ -27,7 +45,7 @@ const Products = ({ gridView, sortedProducts }) => {
         <div className="rounded-[10px] bg-[#f9f1e7] px-5 py-3 md:px-7 md:py-4">
           Next
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
--- src/Pages/SitePages/Shop/Shop.jsx
+++ src/Pages/SitePages/Shop/Shop.jsx
@@ -6,10 +6,36 @@ import SortView from "./SortView/SortView";
 import { useEffect, useState } from "react";
 
 const Shop = () => {
+  const location = useLocation();
   const [products, setProducts] = useState([]);
   const [sortedProducts, setSortedProducts] = useState(products);
   const [gridView, setGridView] = useState(true);
-  const location = useLocation();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const prevDisabled = currentPage === 1;
+  const nextDisabled = currentPage === totalPages;
+
+  const itemsPerPage = 16;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDisplay = sortedProducts.slice(startIndex, endIndex);
+
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   /* get products */
   const getProducts = async () => {
@@ -17,6 +43,7 @@ const Shop = () => {
     const data = await response.json();
     setProducts(data);
     setSortedProducts(data);
+    setTotalPages(Math.ceil(data.length / 16));
   };
 
   /* handle products sorting */
@@ -39,7 +66,12 @@ const Shop = () => {
       {/* page navigation banner */}
       <NavBanner location={location} />
       <SortView handleSort={handleSort} setGridView={setGridView} />
-      <Products gridView={gridView} sortedProducts={sortedProducts} />
+      <Products
+        gridView={gridView}
+        itemsToDisplay={itemsToDisplay}
+        totalPages={totalPages}
+        handlePageChange={handlePageChange}
+      />
       <ServicesHighlight />
     </section>
   );
